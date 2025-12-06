ae40c49f4ec224691f2e8a49dd72cca8fd8d7085
Vinay <vinayadattakavuluri@gmail.com>
2025-12-06 19:49:17 +0530

    configuration changes to use local dockerfile.

--- verify-ui/configure_start.sh
+++ verify-ui/configure_start.sh
@@ -4,11 +4,17 @@ echo "generating env-config file"
 workingDir="$nginx_dir"/html
 
 echo "window._env_ = {" > "${workingDir}"/env.config.js
-awk -F '=' '{
+# strip CR characters from .env (handles Windows line endings), then parse
+sed 's/\r$//' "${workingDir}"/.env | awk -F '=' '{
     key = $1
     value = substr($0, index($0, "=") + 1)
-    print key ": \"" (ENVIRON[key] ? ENVIRON[key] : value) "\","
-}' "${workingDir}"/.env >> "${workingDir}"/env.config.js
+    # trim whitespace from key and value
+    gsub(/^ +| +$/, "", key)
+    gsub(/^ +| +$/, "", value)
+    # escape embedded double quotes in value
+    gsub(/\"/, "\\\"", value)
+    print key ": \"" (ENVIRON[key] ? ENVIRON[key] : value) "\"," 
+}' >> "${workingDir}"/env.config.js
 echo "}" >> "${workingDir}"/env.config.js
 
 echo "generation of env-config file completed!"
